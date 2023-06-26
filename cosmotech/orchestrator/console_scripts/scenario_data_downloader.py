6c6032c170e6670b86e929ce2a0f91a36b0dbebb
Alexis Fossart <alexis.fossart@cosmotech.com>
2023-06-26 14:33:52 +0200

    Added control over type of parameter write + sped up downloader

--- cosmotech/orchestrator/console_scripts/scenario_data_downloader.py
+++ cosmotech/orchestrator/console_scripts/scenario_data_downloader.py
@@ -4,11 +4,14 @@ import json
 import logging
 import os
 import pathlib
+from csv import DictWriter
 from distutils.dir_util import copy_tree
 
 import click_log
 import rich_click as click
 from CosmoTech_Acceleration_Library.Accelerators.scenario_download.scenario_downloader import ScenarioDownloader
+from rich.console import Console
+from rich.style import Style
 from rich.logging import RichHandler
 
 from cosmotech.orchestrator.utils.decorators import require_env
@@ -21,12 +24,14 @@ logging.basicConfig(
     handlers=[RichHandler(rich_tracebacks=True,
                           omit_repeated_times=False,
                           show_path=False,
-                          markup=True)])
+                          markup=True,
+                          console=Console(width=150))])
 LOGGER.setLevel(logging.INFO)
 
 
 def download_scenario_data(
-    organization_id: str, workspace_id: str, scenario_id: str, dataset_folder: str, parameter_folder: str
+    organization_id: str, workspace_id: str, scenario_id: str, dataset_folder: str, parameter_folder: str,
+    write_json: bool, write_csv: bool
 ) -> None:
     """
     Download the datas from a scenario from the CosmoTech API to the local file system
@@ -41,53 +46,66 @@ def download_scenario_data(
     dl = ScenarioDownloader(workspace_id=workspace_id,
                             organization_id=organization_id,
                             read_files=False)
-    LOGGER.info("Initialized downloader")
-    content = dict()
-    content['datasets'] = dl.get_all_datasets(scenario_id=scenario_id)
 
-    content['parameters'] = dl.get_all_parameters(scenario_id=scenario_id)
-    LOGGER.info("Downloaded content")
-    dataset_paths = dict()
+    LOGGER.info("Load scenario data")
+    scenario_data = dl.get_scenario_data(scenario_id=scenario_id)
+
+    LOGGER.info("Download datasets")
+    datasets = dl.get_all_datasets(scenario_id=scenario_id)
+    datasets_parameters_ids = {param.get('value'): param.get('parameter_id')
+                               for param in scenario_data.get('parameters_values')
+                               if param.get('var_type') == "%DATASETID%"}
 
     dataset_dir = dataset_folder
-    pathlib.Path(dataset_dir).mkdir(parents=True, exist_ok=True)
-    for k in content['datasets'].keys():
-        dataset_paths[k] = dl.dataset_to_file(k, content['datasets'][k])
-        if k not in content['parameters'].values():
-            copy_tree(dataset_paths[k], dataset_dir)
-
-    LOGGER.info("Stored datasets")
     tmp_parameter_dir = parameter_folder
+
+    LOGGER.info("Store datasets")
+    pathlib.Path(dataset_dir).mkdir(parents=True, exist_ok=True)
+    for k in datasets.keys():
+        if k not in datasets_parameters_ids.keys():
+            copy_tree(dl.dataset_to_file(k, datasets[k]), dataset_dir)
+            LOGGER.debug(f"  - Stored {dataset_dir} ({k})")
+        else:
+            param_dir = os.path.join(tmp_parameter_dir, datasets_parameters_ids[k])
+            pathlib.Path(param_dir).mkdir(exist_ok=True, parents=True)
+            copy_tree(dl.dataset_to_file(k, datasets[k]), param_dir)
+            LOGGER.debug(f"  - Stored {param_dir} ({k})")
+
     pathlib.Path(tmp_parameter_dir).mkdir(parents=True, exist_ok=True)
 
-    tmp_parameter_file = os.path.join(tmp_parameter_dir, "parameters.json")
+    LOGGER.info("Prepare parameters")
+
+    if not (write_csv or write_json):
+        LOGGER.info("No parameters write asked, skipping")
+        return
 
     parameters = []
-
-    for parameter_name, value in content['parameters'].items():
-        def add_file_parameter(compared_parameter_name: str, dataset_id: str):
-            if parameter_name == compared_parameter_name:
-                param_dir = os.path.join(tmp_parameter_dir, compared_parameter_name)
-                pathlib.Path(param_dir).mkdir(exist_ok=True)
-                dataset_content_path = dataset_paths[dataset_id]
-                copy_tree(dataset_content_path, param_dir)
-                parameters.append({
-                    "parameterId": parameter_name,
-                    "value": parameter_name,
-                    "varType": "%DATASETID%"
-                })
-
-        if value in content['datasets']:
-            add_file_parameter(parameter_name, value)
+    for parameter_data in scenario_data['parameters_values']:
+        parameter_name = parameter_data.get('parameter_id')
+        value = parameter_data.get('value')
+        var_type = parameter_data.get('var_type')
+        is_inherited = parameter_data.get('is_inherited')
         parameters.append({
             "parameterId": parameter_name,
             "value": value,
-            "varType": str(type(value).__name__)
+            "varType": var_type,
+            "isInherited": is_inherited
         })
+        LOGGER.debug(f"  - Prepared {parameter_name} : \"{value}\" (type: {var_type} - is_inherited: {is_inherited})")
 
-    with open(tmp_parameter_file, "w") as _file:
-        json.dump(parameters, _file)
-    LOGGER.info("Generated parameters.json")
+    if write_csv:
+        tmp_parameter_file = os.path.join(tmp_parameter_dir, "parameters.csv")
+        LOGGER.info(f"Generating {tmp_parameter_file}")
+        with open(tmp_parameter_file, "w") as _file:
+            _w = DictWriter(_file, fieldnames=["parameterId", "value", "varType", "isInherited"])
+            _w.writeheader()
+            _w.writerows(parameters)
+
+    if write_json:
+        tmp_parameter_file = os.path.join(tmp_parameter_dir, "parameters.json")
+        LOGGER.info(f"Generating {tmp_parameter_file}")
+        with open(tmp_parameter_file, "w") as _file:
+            json.dump(parameters, _file)
 
 
 @click.command()
@@ -121,6 +139,16 @@ def download_scenario_data(
               show_envvar=True,
               help="A local folder to store the parameters content",
               required=True)
+@click.option("--write-json/--no-write-json",
+              envvar="WRITE_JSON",
+              show_envvar=True,
+              default=False,
+              help="Toggle writing of parameters in json format")
+@click.option("--write-csv/--no-write-csv",
+              envvar="WRITE_CSV",
+              show_envvar=True,
+              default=True,
+              help="Toggle writing of parameters in csv format")
 @click_log.simple_verbosity_option(LOGGER,
                                    "--log-level",
                                    envvar="LOG_LEVEL",
@@ -128,7 +156,9 @@ def download_scenario_data(
 @require_env('CSM_API_SCOPE', "The identification scope of a Cosmotech API")
 @require_env('CSM_API_URL', "The URL to a Cosmotech API")
 def main(
-    scenario_id: str, workspace_id: str, organization_id: str, dataset_absolute_path: str, parameters_absolute_path: str
+    scenario_id: str, workspace_id: str, organization_id: str, dataset_absolute_path: str,
+    parameters_absolute_path: str,
+    write_json: bool, write_csv: bool
 ):
     """
 Uses environment variables to call the download_scenario_data function
@@ -136,7 +166,8 @@ Requires a valid Azure connection either with:
 - The AZ cli command: **az login**
 - A triplet of env var `AZURE_TENANT_ID`, `AZURE_CLIENT_ID`, `AZURE_CLIENT_SECRET`
     """
-    download_scenario_data(organization_id, workspace_id, scenario_id, dataset_absolute_path, parameters_absolute_path)
+    return download_scenario_data(organization_id, workspace_id, scenario_id, dataset_absolute_path,
+                                  parameters_absolute_path, write_json, write_csv)
 
 
 if __name__ == "__main__":
